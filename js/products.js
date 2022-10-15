4a78704399f6366d6d5053a4402a2688ed84aa18
Gaston Romero <romero_gaston_@hotmail.com>
2022-10-15 00:37:01 -0300

    trabajando en el disenio

---
+++ js/products.js
@@ -0,0 +1,32 @@
+class Productos{
+    constructor(id,nombre,detalle,precio,stock,iva,cat,img){
+    this.id = parseInt(id);
+    this.nombre = nombre.toUpperCase();
+    this.detalle = detalle;
+    this.precio = parseFloat(precio);
+    this.stock = parseInt(stock);
+    this.iva = parseFloat(iva);
+    this.cat = cat.toUpperCase();
+    this.img = img;
+    }
+    }
+const listaProducto =[] //TRABAJANDO EN EL LISTADO DE PRODUCTOS EN UN ARRAY
+listaProducto.push(new Productos('1',`Carpa canadiese`,'Carpa canadiese 6 personas',165035,15,1.21,`carpas`,`../IMG/IMG_TIENDA/articulos/1 (9).jpg`));
+
+listaProducto.push(new Productos('2',`Linterna Spinit`,`Linterna Spinit HL800R Frontal/Mano`,6198.1,30,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/1 (8).jpg`));
+
+listaProducto.push(new Productos('3',`Navaja Trento`,`Navaja Trento Hunter Light 1.20`,6500,50,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/1 (2).jpg`));
+
+listaProducto.push(new Productos('4',`Mochila Bamboo`,`Mochila Bamboo Adventure 70 Lts`,45000,53,1.21,`mochilas`,`../IMG/IMG_TIENDA/articulos/1 (5).jpg`));
+
+listaProducto.push(new Productos(`5`,`Vaso Térmico`,"Vaso Térmico Waterdog AB-2035",6300,260,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/ali-kazal-QKCo0sBAm58-unsplash 1.jpg`));
+
+listaProducto.push(new Productos(`6`,`Linterna Ledlenser`,"Linterna Ledlenser MT6 600 Lumens",25300,1030,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/1 (7).jpg`));
+
+listaProducto.push(new Productos(`7`,`Termo Bala`,"Termo Bala Lexus 1 lt.",6300,230,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/1 (6).jpg`));
+
+listaProducto.push(new Productos(`8`,`Termo Contigo`,"Termo Contigo De Acero Inoxidable 1.1 Lts",18500,350,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/1 (4).jpg`));
+
+listaProducto.push(new Productos(`9`,`Carpa Baño`,"Carpa Baño Waterdog Tenth Bath",120000,30,1.21,`carpas`,`../IMG/IMG_TIENDA/articulos/1 (3).jpg`));
+
+listaProducto.push(new Productos(`10`,`Caña Seabay`,"Caña Seabay TournamentS 4.26 Mts",65300,150,1.21,`pesca`,`../IMG/IMG_TIENDA/articulos/1 (1).jpg`));
--- js/tienda.mjs
+++ js/tienda.mjs
@@ -3,31 +3,8 @@ let listaProductoStock = [];
 let listadoNombresProductos = [];
 let listadoNombresID = [];
 let listaCategoria = []
-
-
-class Productos{
-constructor(id,nombre,detalle,precio,stock,iva,cat,img){
-this.id = parseInt(id);
-this.nombre = nombre.toUpperCase();
-this.detalle = detalle;
-this.precio = parseFloat(precio);
-this.stock = parseInt(stock);
-this.iva = parseFloat(iva);
-this.cat = cat.toUpperCase();
-this.img = img;
-}
-}
-const listaProducto =[] //TRABAJANDO EN EL LISTADO DE PRODUCTOS EN UN ARRAY
-listaProducto.push(new Productos('1',`Carpa canadiese`,'Carpa canadiese 6 personas',165035,15,1.21,`carpas`));
-listaProducto.push(new Productos('2',`Linterna Spinit`,`Linterna Spinit HL800R Frontal/Mano`,6198.1,30,1.21,`camping`));
-listaProducto.push(new Productos('3',`Navaja Trento`,`Navaja Trento Hunter Light 1.20`,6500,50,1.21,`camping`));
-listaProducto.push(new Productos('4',`Mochila Bamboo`,`Mochila Bamboo Adventure 70 Lts`,45000,53,1.21,`mochilas`));
-listaProducto.push(new Productos(`5`,`Vaso Térmico`,"Vaso Térmico Waterdog AB-2035",6300,260,1.21,`camping`));
-listaProducto.push(new Productos(`6`,`Linterna Ledlenser`,"Linterna Ledlenser MT6 600 Lumens",25300,1030,1.21,`camping`));
-listaProducto.push(new Productos(`7`,`Termo Bala`,"Termo Bala Lexus 1 lt.",6300,230,1.21,`camping`));
-listaProducto.push(new Productos(`8`,`Termo Contigo`,"Termo Contigo De Acero Inoxidable 1.1 Lts",18500,350,1.21,`camping`));
-listaProducto.push(new Productos(`9`,`Carpa Baño`,"Carpa Baño Waterdog Tenth Bath",120000,30,1.21,`carpas`));
-listaProducto.push(new Productos(`10`,`Caña Seabay`,"Caña Seabay Tournament TRMSS 4262 S (Frontal) 4.26 Mts",65300,150,1.21,`pesca`));
+let carrito = [];
+let priceMoney = `ARS $`
 
 
 listaProductoStock = listaProducto.filter((prod) => prod.stock > 0);
@@ -39,36 +16,62 @@ console.log(listadoNombresProductos); //Si los productos no tienen stock no apar
 listadoNombresID = listaProductoStock.map((prod) => prod.id);
 console.log(listadoNombresID); //Crea un array para los id unicamente
 
-listaCategoria = listaProductoStock.filter((prod)=> prod.cat == listaCategoria);
+listaCategoria = listaProductoStock.filter((prod) => prod.cat == listaCategoria);
 console.log(listaCategoria);
 
 
 
+listaProducto.forEach((prod) => {
+    let card = document.createElement(`div`);
+    let catalogo = document.getElementById(`catalogo`)
 
-for(const prod of listaProductoStock){
-let catalogo = document.getElementById(`catalogo`)
-let card = document.createElement(`div`);
-// card.className = `` ME SIRVE PARA DARLE UNA CLASE
-card.innerHTML= `<div class="conte">
-<div class="card">
-    <img src="../IMG/IMG_TIENDA/articulos/ali-kazal-QKCo0sBAm58-unsplash 1.jpg" alt="imagen de un cuchillo acero damasco">
-    <div class="informacion">
-        <h1 class="tittle_1">${prod.nombre}</h1>
-        <p class="tittle_2"><h6>${prod.detalle}</h6></p>
-    </div>
-    <div class="precio">
-        <div class="box-precio">
-            <span class="precio"><b>$${prod.precio}</b></span>
-            <form name="miform" id="miform" method="get">
-            <input type="number" name="minumber" id="minumber" placeholder="Cantidad" min=0 max="${prod.stock}" step="${prod.stock} + 1">
-            <input type="button" id="miBoton"  class="miBoton"  value="Añadir al carrito"></form
+    card.className = `card`
+    card.innerHTML = `  
+        <img src="${prod.img}" alt="imagen de ${prod.nombre}">
+        <div class="informacion">
+            <h1 class="tittle_1">${prod.nombre}</h1>
+            <p class="tittle_2"><h6>
+            ${prod.detalle}</h6></p>
         </div>
-    </div>
-</div>
-</div>`
-catalogo.append(card);
-}
+        <div class="precio">
+            <div class="box-precio">
+                <span class="precio"><b>${priceMoney} ${prod.precio}</b></span>
+            </div>
+        </div>`;
+    catalogo.append(card);
 
-// accion de boton añadir a carrito
-let miBoton = document.getElementById("miBoton");
-miBoton.addEventListener("click",()=>console.log("Hiciste click"));
+    let botonCompra = document.createElement(`button`);
+    botonCompra.textContent = `Añadir al carrito`;
+    botonCompra.className = "miBoton";
+    card.append(botonCompra)
+
+});
+
+listaProducto.forEach((prod)=>{
+    //BODY CARD
+    let cardBody = document.createElement(`div`);
+    cardBody.classList.add(`card`);
+    //IMG 
+    let imgCard = document.createElement(`img`);
+    imgCard.classList.add(imgCard);
+    imgCard.innerText = prod.img;
+    //NAME PRODUCT
+    let nameProduct = documet.createElement(`div`);
+    nameProduct.classList.add(`tittle_2`);
+    nameProduct.innerText = prod.nombre;
+    //DETAIL PRODUCT
+    let detailProduct = documet.createElement(`div`);
+    detailProduct.classList.add(`tittle_1`);
+    detailProduct.innerText = prod.detalle;
+    //PRICEPRODUCT
+    let priceProduct = documet.createElement(`div`);
+    priceProduct.classList.add(`precio`);
+    priceProduct.innerText = priceMoney + prod.precio;
+
+    cardBody.append(cardBody);
+    img.append(imgCard);
+    catalogo.append(nameProduct);
+    catalogo.append(detailProduct);
+    catalogo.append(priceProduct);
+});
+
