6fa02bdeccb708fe0c1d840f7464adcac67fd0d4
Gaston Romero <romero_gaston_@hotmail.com>
2022-10-15 22:33:43 -0300

    trabajando en workshop

--- js/tienda.mjs
+++ js/tienda.mjs
@@ -2,58 +2,20 @@
 let listaProductoStock = [];
 let listadoNombresProductos = [];
 let listadoNombresID = [];
-let listaCategoria = []
 let cart = [];
 let priceMoney = `ARS $`
 let catalogo = document.getElementById("catalogo");
-let CartList = document.getElementById("carritoPrincipal")
+let cartList = document.getElementById("carritoPrincipal")
 
-const listaProducto =[] //TRABAJANDO EN EL LISTADO DE PRODUCTOS EN UN ARRAY
-class Productos{
-    constructor(id,nombre,detalle,precio,stock,iva,cat,img){
-    this.id = parseInt(id);
-    this.nombre = nombre.toUpperCase();
-    this.detalle = detalle;
-    this.precio = parseFloat(precio);
-    this.stock = parseInt(stock);
-    this.iva = parseFloat(iva);
-    this.cat = cat.toUpperCase();
-    this.img = img;
-    }
-    }
-
-listaProducto.push(new Productos('1',`Carpa canadiese`,'Carpa canadiese 6 personas',165035,15,1.21,`carpas`,`../IMG/IMG_TIENDA/articulos/1 (9).jpg`));
-
-listaProducto.push(new Productos('2',`Linterna Spinit`,`Linterna Spinit HL800R Frontal/Mano`,6198.1,30,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/1 (8).jpg`));
-
-listaProducto.push(new Productos('3',`Navaja Trento`,`Navaja Trento Hunter Light 1.20`,6500,50,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/1 (2).jpg`));
-
-listaProducto.push(new Productos('4',`Mochila Bamboo`,`Mochila Bamboo Adventure 70 Lts`,45000,53,1.21,`mochilas`,`../IMG/IMG_TIENDA/articulos/1 (5).jpg`));
-
-listaProducto.push(new Productos(`5`,`Vaso Térmico`,"Vaso Térmico Waterdog AB-2035",6300,260,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/ali-kazal-QKCo0sBAm58-unsplash 1.jpg`));
-
-listaProducto.push(new Productos(`6`,`Linterna Ledlenser`,"Linterna Ledlenser MT6 600 Lumens",25300,1030,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/1 (7).jpg`));
-
-listaProducto.push(new Productos(`7`,`Termo Bala`,"Termo Bala Lexus 1 lt.",6300,230,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/1 (6).jpg`));
-
-listaProducto.push(new Productos(`8`,`Termo Contigo`,"Termo Contigo De Acero Inoxidable 1.1 Lts",18500,350,1.21,`camping`,`../IMG/IMG_TIENDA/articulos/1 (4).jpg`));
-
-listaProducto.push(new Productos(`9`,`Carpa Baño`,"Carpa Baño Waterdog Tenth Bath",120000,30,1.21,`carpas`,`../IMG/IMG_TIENDA/articulos/1 (3).jpg`));
-
-listaProducto.push(new Productos(`10`,`Caña Seabay`,"Caña Seabay TournamentS 4.26 Mts",65300,150,1.21,`pesca`,`../IMG/IMG_TIENDA/articulos/1 (1).jpg`));
 
 listaProductoStock = listaProducto.filter((prod) => prod.stock > 0);
-console.log(listaProductoStock); //Filtra los productos mayores a cero y crea un array
+//Filtra los productos mayores a cero y crea un array
 
 listadoNombresProductos = listaProductoStock.map((prod) => prod.nombre);
-console.log(listadoNombresProductos); //Si los productos no tienen stock no aparecen y crea un array
+//Si los productos no tienen stock no aparecen y crea un array
 
 listadoNombresID = listaProductoStock.map((prod) => prod.id);
-console.log(listadoNombresID); //Crea un array para los id unicamente
-
-listaCategoria = listaProductoStock.filter((prod) => prod.cat == listaCategoria);
-console.log(listaCategoria);
-
+//Crea un array para los id unicamente
 
 listaProducto.forEach((prod)=>{
     //BODY CARD
@@ -92,19 +54,50 @@ listaProducto.forEach((prod)=>{
 
 });
 
+
+
+
 function addProdToCart(e){
     cart.push(e.target.getAttribute(`mark`));
-    console.log(cart);
     renderCart()
 }
+
+
+
+
 function renderCart(){
+    
+    cartList.innerHTML = "";
 
     let cartsRepetidas = [...new Set(cart)]
 
-    cartsRepetidas.forEach((prod) => {
+    cartsRepetidas.forEach((itemId) => {
         let item = listaProducto.filter((produc) =>{
-            return produc.id === parseInt(prod)
+            return produc.id === parseInt(itemId)
         })
-        console.log(item)
-    })
+        let quantity = cart.reduce((total, id) =>{
+            return id === itemId ? total += 1 : total
+        },0)
+
+
+
+    
+    let linea = document.createElement(`li`,`p`);
+    linea.classList.add("carrito");
+    linea.innerText =` ${quantity} x ${item[0].nombre} - $${item[0].precio}`;
+
+
+    let buttonDelete = document.createElement(`button`);
+    buttonDelete.classList.add(`buttonDelete`);
+    buttonDelete.innerText = `Eliminar item`;
+    buttonDelete.dataset.item = itemId
+    //buttonDelete.addEventListener(`click`, deleteProduc);
+
+
+    linea.append(buttonDelete);
+    cartList.append(linea);
+    cartList.append(imgCard);
+
+})
+
 }
