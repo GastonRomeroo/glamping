394f6cbb8b9486c9782838023603aeaa2b432e5b
Gaston Romero <romero_gaston_@hotmail.com>
2022-11-05 20:14:32 -0300

    trabajando

--- js/index.js
+++ js/index.js
@@ -5,3 +5,10 @@ toggle.onclick = function(){
 };
 
 
+//LOCAL STORAGE DEL USUARIO
+function loadLoginFromStorage(){
+    if(localStorage.getItem(`login`) !== null){
+    login = JSON.parse(localStorage.getItem(`login`));
+    document.querySelector(".singin").style.display = "none";
+    return login};
+  }
--- js/tienda.js
+++ js/tienda.js
@@ -170,3 +170,24 @@ function calculateTotalPrice(){
 
 
 
+// Ventana modal
+let modal = document.getElementById("ventanaModal");
+// Botón que abre el modal
+let boton = document.getElementById("abrirModal");
+// Hace referencia al elemento <span> que tiene la X que cierra la ventana
+let span = document.getElementsByClassName("cerrar")[0];
+
+// Cuando el usuario hace clic en el botón, se abre la ventana
+boton.addEventListener("click",function() {
+    modal.style.display = "block";
+  });
+  // Si el usuario hace clic en la x, la ventana se cierra
+  span.addEventListener("click",function() {
+    modal.style.display = "none";
+  });
+  // Si el usuario hace clic fuera de la ventana, se cierra.
+  window.addEventListener("click",function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
